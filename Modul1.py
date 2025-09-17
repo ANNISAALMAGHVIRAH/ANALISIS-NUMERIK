90a1530fac8ed546a75ab5e188b2e7677ec68b29
ANNISA ALMAGHVIRAH <annisaalmaghvirah332@gmail.com>
2025-09-17 12:09:02 +0800

    Create Modul1.py

---
+++ Modul1.py
@@ -0,0 +1,34 @@
+#Algoritma1
+#Step 1: input awal adalah angka yang paling besar
+x_awal1 = 10000.0
+
+#Step 2: input awal adalah angka yang paling kecil
+x_selanjutnya1 = 0.0001
+
+#Step 3: melakukan perulangan
+for _ in range(5):
+  x_awal1 += x_selanjutnya1
+
+#cetak hasil
+print("Hasil penjumlahan algoritma 1 adalah", x_awal1)
+
+
+
+
+
+
+#algoritna2
+
+#Total2
+Total2 =0.0
+
+#input nilai awal adalah angka yang paling kecil
+x_awal2 = 0.0001
+
+#melakukan perulangan sebanyak 5 kali
+for _ in range (5):
+  Total2 += x_awal2
+Total2 += 1000.0
+
+#cetak hasil
+print("Hasil penjumlahan algoritma 2 adalah", Total2)
