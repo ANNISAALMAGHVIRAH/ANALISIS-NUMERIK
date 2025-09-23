dbd4bac0f6c19eaf30d92ee2dd72177595a5ff02
ANNISA ALMAGHVIRAH <annisaalmaghvirah332@gmail.com>
2025-09-23 12:49:02 +0800

    Update Modul1.py

--- Modul1.py
+++ Modul1.py
@@ -1,34 +1,30 @@
-#Algoritma1
-#Step 1: input awal adalah angka yang paling besar
-x_awal1 = 10000.0
+# Hasil sebenarnya
+H_sebenarnya = 100001.0
 
-#Step 2: input awal adalah angka yang paling kecil
-x_selanjutnya1 = 0.0001
+# Algoritma 1
+#step 1: Input nilai awal adalah angka yang paling besar
+x_awal1 = 100000.0
+#Step 2: input nilai selanjutnya adalah angka yang kecil
+x_selanjutnnya1 = 0.00001
+# Step 3: Melakukan perulangan
+for _ in range (100000):
+  x_awal1 += x_selanjutnnya1
+# Cetak hasil
 
-#Step 3: melakukan perulangan
-for _ in range(5):
-  x_awal1 += x_selanjutnya1
-
-#cetak hasil
-print("Hasil penjumlahan algoritma 1 adalah", x_awal1)
-
-
-
-
-
-
-#algoritna2
-
-#Total2
-Total2 =0.0
-
-#input nilai awal adalah angka yang paling kecil
-x_awal2 = 0.0001
-
-#melakukan perulangan sebanyak 5 kali
-for _ in range (5):
-  Total2 += x_awal2
-Total2 += 1000.0
-
-#cetak hasil
-print("Hasil penjumlahan algoritma 2 adalah", Total2)
+# Algoritma 2
+total = 0
+#step 1: Input nilai awal adalah angka yang paling kecil
+x_awal2  = 0.00001
+#Step 2: input nilai selanjutnya adalah angka yang besar
+x_selanjutnnya2 = 100000.0
+# Step 3: Melakukan perulangan
+for _ in range (100000):
+  total += x_awal2
+total += x_selanjutnnya2
+# Cetak hasil
+print ("Hasil sebenarnya            :",H_sebenarnya)
+print ("Hasil algoritma 1 adalah    :",x_awal1)
+print ("Hasil algoritma 2 adalah    :",total)
+print ("Galat algoritma 1           :",x_awal1-H_sebenarnya)
+print ("Galat algoritma 2           :",total-H_sebenarnya)
+print ("Perbedaan algoritma 1 dan 2 :",x_awal1-total)
